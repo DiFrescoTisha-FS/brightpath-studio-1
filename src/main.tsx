80ced5291ae2121205c6a4d4cd52cc291d6abdb4
Tisha Di Fresco <tbdifresco@student.fullsail.edu>
2026-03-07 18:57:15 -0500

    Added Google Analytics 4

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,10 @@ const SocialMediaPortfolioPage = React.lazy(() => import('./pages/SocialMediaPor
 const CaseStudyPage = React.lazy(() => import('./pages/CaseStudyPage'));
 import "./index.css";
 import { initializeTheme } from "./store/appStore";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-P8N6CYZX9Q");
+ReactGA.send("pageview");
 
 initializeTheme();
 
