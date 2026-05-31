4a539aa8133b6e478d4672fbe5c12160f53014be
Tisha Di Fresco <tbdifresco@student.fullsail.edu>
2026-05-30 20:43:20 -0400

    Add more spacing between portfolio cards on mobile

--- src/pages/PortfolioDemoPage.tsx
+++ src/pages/PortfolioDemoPage.tsx
@@ -127,7 +127,7 @@ export default function PortfolioDemoPage() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-8 max-w-6xl mx-auto">
           <AweStruckCard onViewCaseStudy={() => {
             setView('awestruck');
             window.scrollTo(0, 0);
